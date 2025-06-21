07a9cad0c62189d462998b2937264c3aaa3c12c0
koheimikami3 <k.mikami2232@gmail.com>
2025-06-21 15:14:08 +0900

    refactor: ユーザー関連のfreezedクラス名を変更

--- lib/features/user/domain/create_user_data.freezed.dart
+++ lib/features/user/domain/create_user_data.freezed.dart
@@ -3,7 +3,7 @@
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
-part of 'user_creation_data.dart';
+part of 'create_user_data.dart';
 
 // **************************************************************************
 // FreezedGenerator
@@ -14,48 +14,48 @@ T _$identity<T>(T value) => value;
 final _privateConstructorUsedError = UnsupportedError(
     'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
-UserCreationData _$UserCreationDataFromJson(Map<String, dynamic> json) {
-  return _UserCreationData.fromJson(json);
+CreateUserData _$CreateUserDataFromJson(Map<String, dynamic> json) {
+  return _CreateUserData.fromJson(json);
 }
 
 /// @nodoc
-mixin _$UserCreationData {
+mixin _$CreateUserData {
 // 端末のOS
   String get os => throw _privateConstructorUsedError; // FCMトークン
   String get token => throw _privateConstructorUsedError; // 作成日時
   @CreatedAtField()
   DateTime? get createdAt => throw _privateConstructorUsedError;
 
-  /// Serializes this UserCreationData to a JSON map.
+  /// Serializes this CreateUserData to a JSON map.
   Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
 
-  /// Create a copy of UserCreationData
+  /// Create a copy of CreateUserData
   /// with the given fields replaced by the non-null parameter values.
   @JsonKey(includeFromJson: false, includeToJson: false)
-  $UserCreationDataCopyWith<UserCreationData> get copyWith =>
+  $CreateUserDataCopyWith<CreateUserData> get copyWith =>
       throw _privateConstructorUsedError;
 }
 
 /// @nodoc
-abstract class $UserCreationDataCopyWith<$Res> {
-  factory $UserCreationDataCopyWith(
-          UserCreationData value, $Res Function(UserCreationData) then) =
-      _$UserCreationDataCopyWithImpl<$Res, UserCreationData>;
+abstract class $CreateUserDataCopyWith<$Res> {
+  factory $CreateUserDataCopyWith(
+          CreateUserData value, $Res Function(CreateUserData) then) =
+      _$CreateUserDataCopyWithImpl<$Res, CreateUserData>;
   @useResult
   $Res call({String os, String token, @CreatedAtField() DateTime? createdAt});
 }
 
 /// @nodoc
-class _$UserCreationDataCopyWithImpl<$Res, $Val extends UserCreationData>
-    implements $UserCreationDataCopyWith<$Res> {
-  _$UserCreationDataCopyWithImpl(this._value, this._then);
+class _$CreateUserDataCopyWithImpl<$Res, $Val extends CreateUserData>
+    implements $CreateUserDataCopyWith<$Res> {
+  _$CreateUserDataCopyWithImpl(this._value, this._then);
 
   // ignore: unused_field
   final $Val _value;
   // ignore: unused_field
   final $Res Function($Val) _then;
 
-  /// Create a copy of UserCreationData
+  /// Create a copy of CreateUserData
   /// with the given fields replaced by the non-null parameter values.
   @pragma('vm:prefer-inline')
   @override
@@ -82,25 +82,25 @@ class _$UserCreationDataCopyWithImpl<$Res, $Val extends UserCreationData>
 }
 
 /// @nodoc
-abstract class _$$UserCreationDataImplCopyWith<$Res>
-    implements $UserCreationDataCopyWith<$Res> {
-  factory _$$UserCreationDataImplCopyWith(_$UserCreationDataImpl value,
-          $Res Function(_$UserCreationDataImpl) then) =
-      __$$UserCreationDataImplCopyWithImpl<$Res>;
+abstract class _$$CreateUserDataImplCopyWith<$Res>
+    implements $CreateUserDataCopyWith<$Res> {
+  factory _$$CreateUserDataImplCopyWith(_$CreateUserDataImpl value,
+          $Res Function(_$CreateUserDataImpl) then) =
+      __$$CreateUserDataImplCopyWithImpl<$Res>;
   @override
   @useResult
   $Res call({String os, String token, @CreatedAtField() DateTime? createdAt});
 }
 
 /// @nodoc
-class __$$UserCreationDataImplCopyWithImpl<$Res>
-    extends _$UserCreationDataCopyWithImpl<$Res, _$UserCreationDataImpl>
-    implements _$$UserCreationDataImplCopyWith<$Res> {
-  __$$UserCreationDataImplCopyWithImpl(_$UserCreationDataImpl _value,
-      $Res Function(_$UserCreationDataImpl) _then)
+class __$$CreateUserDataImplCopyWithImpl<$Res>
+    extends _$CreateUserDataCopyWithImpl<$Res, _$CreateUserDataImpl>
+    implements _$$CreateUserDataImplCopyWith<$Res> {
+  __$$CreateUserDataImplCopyWithImpl(
+      _$CreateUserDataImpl _value, $Res Function(_$CreateUserDataImpl) _then)
       : super(_value, _then);
 
-  /// Create a copy of UserCreationData
+  /// Create a copy of CreateUserData
   /// with the given fields replaced by the non-null parameter values.
   @pragma('vm:prefer-inline')
   @override
@@ -109,7 +109,7 @@ class __$$UserCreationDataImplCopyWithImpl<$Res>
     Object? token = null,
     Object? createdAt = freezed,
   }) {
-    return _then(_$UserCreationDataImpl(
+    return _then(_$CreateUserDataImpl(
       os: null == os
           ? _value.os
           : os // ignore: cast_nullable_to_non_nullable
@@ -128,13 +128,13 @@ class __$$UserCreationDataImplCopyWithImpl<$Res>
 
 /// @nodoc
 @JsonSerializable()
-class _$UserCreationDataImpl extends _UserCreationData {
-  const _$UserCreationDataImpl(
+class _$CreateUserDataImpl extends _CreateUserData {
+  const _$CreateUserDataImpl(
       {required this.os, required this.token, @CreatedAtField() this.createdAt})
       : super._();
 
-  factory _$UserCreationDataImpl.fromJson(Map<String, dynamic> json) =>
-      _$$UserCreationDataImplFromJson(json);
+  factory _$CreateUserDataImpl.fromJson(Map<String, dynamic> json) =>
+      _$$CreateUserDataImplFromJson(json);
 
 // 端末のOS
   @override
@@ -149,14 +149,14 @@ class _$UserCreationDataImpl extends _UserCreationData {
 
   @override
   String toString() {
-    return 'UserCreationData(os: $os, token: $token, createdAt: $createdAt)';
+    return 'CreateUserData(os: $os, token: $token, createdAt: $createdAt)';
   }
 
   @override
   bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
-            other is _$UserCreationDataImpl &&
+            other is _$CreateUserDataImpl &&
             (identical(other.os, os) || other.os == os) &&
             (identical(other.token, token) || other.token == token) &&
             (identical(other.createdAt, createdAt) ||
@@ -167,32 +167,32 @@ class _$UserCreationDataImpl extends _UserCreationData {
   @override
   int get hashCode => Object.hash(runtimeType, os, token, createdAt);
 
-  /// Create a copy of UserCreationData
+  /// Create a copy of CreateUserData
   /// with the given fields replaced by the non-null parameter values.
   @JsonKey(includeFromJson: false, includeToJson: false)
   @override
   @pragma('vm:prefer-inline')
-  _$$UserCreationDataImplCopyWith<_$UserCreationDataImpl> get copyWith =>
-      __$$UserCreationDataImplCopyWithImpl<_$UserCreationDataImpl>(
+  _$$CreateUserDataImplCopyWith<_$CreateUserDataImpl> get copyWith =>
+      __$$CreateUserDataImplCopyWithImpl<_$CreateUserDataImpl>(
           this, _$identity);
 
   @override
   Map<String, dynamic> toJson() {
-    return _$$UserCreationDataImplToJson(
+    return _$$CreateUserDataImplToJson(
       this,
     );
   }
 }
 
-abstract class _UserCreationData extends UserCreationData {
-  const factory _UserCreationData(
+abstract class _CreateUserData extends CreateUserData {
+  const factory _CreateUserData(
       {required final String os,
       required final String token,
-      @CreatedAtField() final DateTime? createdAt}) = _$UserCreationDataImpl;
-  const _UserCreationData._() : super._();
+      @CreatedAtField() final DateTime? createdAt}) = _$CreateUserDataImpl;
+  const _CreateUserData._() : super._();
 
-  factory _UserCreationData.fromJson(Map<String, dynamic> json) =
-      _$UserCreationDataImpl.fromJson;
+  factory _CreateUserData.fromJson(Map<String, dynamic> json) =
+      _$CreateUserDataImpl.fromJson;
 
 // 端末のOS
   @override
@@ -203,10 +203,10 @@ abstract class _UserCreationData extends UserCreationData {
   @CreatedAtField()
   DateTime? get createdAt;
 
-  /// Create a copy of UserCreationData
+  /// Create a copy of CreateUserData
   /// with the given fields replaced by the non-null parameter values.
   @override
   @JsonKey(includeFromJson: false, includeToJson: false)
-  _$$UserCreationDataImplCopyWith<_$UserCreationDataImpl> get copyWith =>
+  _$$CreateUserDataImplCopyWith<_$CreateUserDataImpl> get copyWith =>
       throw _privateConstructorUsedError;
 }
