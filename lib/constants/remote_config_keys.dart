09c11c29e7c7e3e371b7e1fa5566c1865160da5b
koheimikami3 <k.mikami2232@gmail.com>
2025-06-26 13:27:30 +0900

    refactor: RemoteConfigキーを管理する定数クラスを作成

---
+++ lib/constants/remote_config_keys.dart
@@ -0,0 +1,10 @@
+/// RemoteConfigのキーを管理する定数クラス
+class RemoteConfigKeys {
+  RemoteConfigKeys._();
+
+  // iOS版強制アップデートバージョン
+  static const iOSForcedAppVersionRemoteKey = 'forceAppVersionIOS';
+
+  // Android版強制アップデートバージョン
+  static const androidForcedAppVersionRemoteKey = 'forceAppVersionAndroid';
+}
