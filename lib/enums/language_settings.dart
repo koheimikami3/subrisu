14ad618d8eb68cb6fea0a076fe5a509b2d92d12f
koheimikami3 <k.mikami.developer@gmail.com>
2025-06-26 10:21:25 +0900

    refactor: 言語設定を管理するSharedPreferencesのキーバリューを変更
    バリューはEnumクラスのインデックスではなく言語コードとする

--- lib/enums/language_settings.dart
+++ lib/enums/language_settings.dart
@@ -6,4 +6,11 @@ enum LanguageSettings {
   const LanguageSettings(this.code);
 
   final String code;
+
+  /// 言語コードから対応するLanguageSettingsを取得
+  static LanguageSettings fromCode(String code) {
+    return LanguageSettings.values.firstWhere(
+      (language) => language.code.toLowerCase() == code.toLowerCase(),
+    );
+  }
 }
